435b060afe5d3dc740d0bbc2dcc90ad38d64e39e
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2021-08-16 15:24:39 +0700

    Add files via upload

--- follower_scraper_work.py
+++ follower_scraper_work.py
@@ -10,6 +10,9 @@ username = input("Input your instagram username: ")
 password = stdiomask.getpass(prompt="Input your instagram password: ", mask="X")
 L.login(username, password)  #(login)
 
+print("\n")
+print("Login to " + username + " account. . . . .")
+
 #Save list
 def save_list(data, filename):
     print("\n")
@@ -26,18 +29,27 @@ def save_list(data, filename):
 profile = instaloader.Profile.from_username(L.context, username)
 
 #Get list of followers
+print("\n")
+print("Getting the list of followers (people that follow). . . . .")
+
 followers_list = []
 for followers in profile.get_followers():
     followers_list.append(followers.username)
 save_list(followers_list, "followers")
 
 #Get list of followees
+print("\n")
+print("Getting the list of followees (people that followed). . . . .")
+
 followees_list = []
 for followees in profile.get_followees():
     followees_list.append(followees.username)
 save_list(followees_list, "followees")
 
 #Get list of unfollowers
+print("\n")
+print("Getting the list of unfollowers (people that followed, but not follow back). . . . .")
+
 unfollowers_list = []
 for followees in followees_list:
     if followees not in followers_list:
@@ -45,6 +57,9 @@ for followees in followees_list:
 save_list(unfollowers_list, "unfollowers")
 
 #Get list of unfollowees
+print("\n")
+print("Getting the list of unfollowees (people that follow, but not followed back). . . . .")
+
 unfollowees_list = []
 for followers in followers_list:
     if followers not in followees_list:
@@ -60,3 +75,5 @@ print("Number of Unfollowers: " + str(len(unfollowers_list)))
 print("\n")
 print("Number of Unfollowees: " + str(len(unfollowees_list)))
 print("\n")
+
+input("Press enter to close. . . . .")
